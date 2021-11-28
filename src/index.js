2df6c4d8d7f6d6bfa0b8d29a54c38d74fc1c7403
Doston <doston.sanatovich@gmail.com>
2021-11-28 16:34:37 -0500

    firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('hi from console');
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBF5eFk3pVirGAVC6Q-cqb0v1QnB3b4DXU",
+    authDomain: "fir-dojo-a15b2.firebaseapp.com",
+    projectId: "fir-dojo-a15b2",
+    storageBucket: "fir-dojo-a15b2.appspot.com",
+    messagingSenderId: "824838201987",
+    appId: "1:824838201987:web:029a7d3aa02cae654e71b6"
+  };
+
+  // init Firebase 
+  initializeApp(firebaseConfig);
+
+  // init Firestore 
+  const db = getFirestore();
+
+
+
